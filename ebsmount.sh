075e99ebf8bd24114f8887113aae6bbeba02626f
Isuru Wickramarathne <isuru.yw@gmail.com>
2017-11-15 09:55:59 +0530

    shell script for mount ebs volumes

--- ebsmount.sh
+++ ebsmount.sh
@@ -3,7 +3,7 @@
 #getting instance az
 abzone=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
-#getting availble ebs volume-id
+#getting availble ebs  volume-id
 ebsvolume=$(/var/awslogs/bin/aws ec2 describe-volumes --filters Name=tag-value,Values=project Name=tag-value,Values=environment Name=tag-value,Values=product Name=availability-zone,Values=`echo $abzone` --query 'Volumes[*].[VolumeId, State==`available`]' --output text  | grep True | awk '{print $1}' | head -n 1)
 
 #check if there are avaible ebs vloumes
